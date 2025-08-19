b6c939929351f0307eb03d8282df51df64fd9d45
CodeWith-Het <hetp1065@gmail.com>
2025-08-19 12:10:50 -0400

    Added

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Card from './Components/Card'
+// import Hello from './Hello';
+
+const App = () => {
+  return (
+    <div>
+      {/* <Hello /> */}
+    </div>
+  )
+}
+
+export default App
