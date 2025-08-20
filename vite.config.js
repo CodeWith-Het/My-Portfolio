0d843f6ee65594d9d493cb086cee48545e653ebe
CodeWith-Het <hetp1065@gmail.com>
2025-08-20 12:54:50 -0400

    Updated

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: "/My-Portfolio/",   // 👈 yaha repo ka naam 100% sahi hona chahiye
   plugins: [react()],
-  // base: "/React/",  
-  base: "/My-Portfolio/",
 })
