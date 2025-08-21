89f32a51c0226013f45baa7acfba8d18c317c553
CodeWith-Het <hetp1065@gmail.com>
2025-08-21 12:08:25 -0400

    Updated

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,20 @@
 import React from 'react'
 import Navbar from './Components/Navbar/Navbar'
 import Hero from './Components/Hero-Section/Hero'
+import About from './Components/About-Section/About'
+import SkillProject from './Components/Skill+Project-Section/Skill_Project'
+import ContactUs from './Components/Contact/Contact'
+import Footer from './Components/Footer-Section/Footer'
 
 const App = () => {
   return (
     <div>
       <Navbar />
       <Hero />
+      <About />
+      <SkillProject />
+      <ContactUs />
+      <Footer />
     </div>
   )
 }
